c9552d1440e56cf9a89762ed82a0a45c0bdf4163
彦 曲 <yanyanyan@hikos-MacBook-Air.local>
2015-04-12 15:16:12 +0800

    added the chipmunk image and used audioEngine created the high pitched audio

--- PlaySoundsViewController.swift
+++ PlaySoundsViewController.swift
@@ -13,7 +13,8 @@ class PlaySoundsViewController: UIViewController {
 
     var audioPlayer:AVAudioPlayer!
     var receivedAudio:RecordedAudio!
-    
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +28,8 @@ class PlaySoundsViewController: UIViewController {
 //        }
         audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
         audioPlayer.enableRate = true
-        
+        audioEngine = AVAudioEngine()
+        audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -62,6 +64,37 @@ class PlaySoundsViewController: UIViewController {
         audioPlayer.currentTime = 0
         audioPlayer.play()
     }
+    
+    @IBAction func playChipmunkSound(sender: UIButton) {
+        // play chipmunk sound
+        playAudioWithVariablePitch( 1000 )
+        
+    }
+    
+    func playAudioWithVariablePitch(pitch: Float){
+        
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        
+        audioEngine.startAndReturnError(nil)
+        
+        audioPlayerNode.play()
+       
+    }
+    
     /*
     // MARK: - Navigation
 
